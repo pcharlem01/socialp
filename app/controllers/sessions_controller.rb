3644fcfefe3a6ebd23a6a17261cc01338c97e5f1
Philip <pcharlem01@gmail.com>
2014-07-24 00:18:34 -0400

    identity file updated

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+
+  def create
+
+	user = User.from_omniauth(env["omniauth.auth"])
+
+    session[:user_id] = user.id
+
+
+    redirect_to root_url
+
+  end
+
+
+
+  def destroy
+
+    session[:player_id] = nil
+  	redirect_to root_url, :notice => "Signed out!"
+
+  end
+
+end
